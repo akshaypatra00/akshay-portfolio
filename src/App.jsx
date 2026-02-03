dd8a08df6f4169977b381a630dc1bd3b5ee31928
Akshay patra <akshaykumarpatra00@gmail.com>
2026-02-04 00:13:57 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,13 @@ import { memo, lazy, Suspense } from "react";
 import Navbar from "./components/NavBar";
 import Hero from "./sections/Hero";
 import AboutSection from "./sections/AboutSection";
-import FeatureC
+import FeatureCards from "./sections/FeatureCards";
+import RecentProjects from "./components/RecentProjects";
+import Experience from "./sections/Experience";
+import TechStack from "./sections/TechStack";
+import Testimonials from "./sections/Certifications";
+import Contact from "./sections/Contact";
+import Footer from "./sections/Footer";
 
 
 const LazyTestimonials = lazy(() => import("./sections/Certifications"));
